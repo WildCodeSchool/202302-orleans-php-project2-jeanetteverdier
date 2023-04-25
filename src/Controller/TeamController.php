8bdfcc29487b1bb8efcd695a7dc3f8f1f94e5f05
Noemie.BARRE <noemie.barre24@gmail.com>
2023-04-25 09:29:32 +0200

    la page qui affiche l'équipe

---
+++ src/Controller/TeamController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\TeamManager;
+
+class TeamController extends AbstractController
+{
+    public function index(): string
+    {
+        $teamManager = new TeamManager();
+        $teams = $teamManager->mySelectAll();
+        $workDepartements = $teamManager->workDepartement();
+
+        return $this->twig->render('Pages/team.html.twig', ['teams' => $teams, 'workDepartements' => $workDepartements]);
+    }
+
+    
+
+}
