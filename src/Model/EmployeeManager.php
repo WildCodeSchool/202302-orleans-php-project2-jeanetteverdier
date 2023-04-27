46725c0be89b92e4d86f0b667bb48cba9636d5e6
Noemie.BARRE <noemie.barre24@gmail.com>
2023-04-27 17:25:16 +0200

    register some query

--- src/Model/EmployeeManager.php
+++ src/Model/EmployeeManager.php
@@ -17,4 +17,35 @@ class EmployeeManager extends AbstractManager
 
         return  $statement->fetchAll();
     }
+    public function insertEmployee(array $employee): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO "
+            . self::TABLE .
+            " (`firstname`), (`lastname`), (`post`), (`picture`)
+        VALUES (:firstname), (:lastname), (:post), (:picture)");
+        $statement->bindValue('firstname', $employee['firstname'], PDO::PARAM_STR);
+        $statement->bindValue('lastname', $employee['lastname'], PDO::PARAM_STR);
+        $statement->bindValue('post', $employee['post'], PDO::PARAM_STR);
+        $statement->bindValue('picture', $employee['picture'], PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    public function updateEmployee(array $employe): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE .
+            " SET `firstname` = :firstname,
+        `lastname` = :lastname,
+        `post` = :post,
+        `picture` = :picture,
+         WHERE id=:id");
+        $statement->bindValue('id', $employe['id'], PDO::PARAM_INT);
+        $statement->bindValue('firstname', $employe['firstname'], PDO::PARAM_STR);
+        $statement->bindValue('lastname', $employe['lastname'], PDO::PARAM_STR);
+        $statement->bindValue('post', $employe['post'], PDO::PARAM_STR);
+        $statement->bindValue('picture', $employe['picture'], PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
 }
