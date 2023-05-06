62d59d15cb6105643bfc8351aa80bd908e1240fe
Mike Xiong <xiong.tchylon@gmail.com>
2023-05-06 16:14:38 +0200

    ajout partie next_training

--- src/Controller/AdminTrainingController.php
+++ src/Controller/AdminTrainingController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use App\Model\TrainingManager;
 use App\Model\SectorManager;
+use App\Model\TrainingManager;
+use App\Model\TrainingSkillManager;
+use App\Model\TrainingNextTrainingManager;
 
 class AdminTrainingController extends AbstractController
 {
@@ -11,15 +13,21 @@ class AdminTrainingController extends AbstractController
     {
         $trainingManager = new TrainingManager();
         $sectorManager = new SectorManager();
+        $skillManager = new TrainingSkillManager();
+        $nextTrainingManager = new TrainingNextTrainingManager();
 
         $trainings = $trainingManager->selectAllTraining();
         $sectors = $sectorManager->selectAllSectors();
+        $skills = $skillManager->selectAllSkills();
+        $nextTrainings = $nextTrainingManager->selectAdminAllNextTraining();
 
         return $this->twig->render(
             'Admin/Training/index.html.twig',
             [
                 'trainings' => $trainings,
                 'sectors' => $sectors,
+                'skills' => $skills,
+                'nextTrainings' => $nextTrainings,
             ]
         );
     }
